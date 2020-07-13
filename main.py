c8b8d37731e727699375298720c99f240aff2d31
Doron Meshulam <doronmmm@gmail.com>
2020-07-13 23:38:22 +0300

    ascii art

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import boards
 from soduko import Soduko
 
 if __name__ == "__main__":
+    print(
+ r''' ___,   ____,   ____,    _,  _,  _,  _,  ____, 
+///=\\ ///=\\\ /||=\\\  /|| /|| /|| /// ///=\\\
+\\\_,  ||| ||| |||  ||| ||| ||| |||///  ||| |||
+_`"\\\ ||| ||| |||  ||| ||| ||| |||\\\  ||| |||
+\\=/// \\\=/// |||=///  \\\=/// ||| \\\ \\\=///
+ `""`   `"""`  `"""""`   `"""`  `"`  `"` `"""` 
+                    @mdoron''')
     print("[*] Solving...")
     solved_board = Soduko.solve(boards.board4)
     if solved_board is None:
